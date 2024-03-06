c125ec9a6e7cf13f0b395097a8fa5a17d6cfc07d
A <monica.a@capgemini.com>
2024-03-06 12:54:10 +0530

    Frist commit in second branch

--- GitDemo/src/gitdemo/GitHelloWorld.java
+++ GitDemo/src/gitdemo/GitHelloWorld.java
@@ -2,7 +2,8 @@ package gitdemo;
 
 public class GitHelloWorld {
 	public static void main(String[] args) {
-        System.out.println("Hello All! Welcome to Git! I am monica"); 
+        System.out.println("Hello ! Welcome to Git");
+        System.out.println("Let us learn git");
         
     }
 }
