0db6e0611291a4e58795c3296a8403bd60d735a9
A <monica.a@capgemini.com>
2024-03-06 12:48:16 +0530

    Second Commit

--- GitDemo/src/gitdemo/GitHelloWorld.java
+++ GitDemo/src/gitdemo/GitHelloWorld.java
@@ -2,6 +2,7 @@ package gitdemo;
 
 public class GitHelloWorld {
 	public static void main(String[] args) {
-        System.out.println("Welcome to Git HelloWorld!"); 
+        System.out.println("Hello All! Welcome to Git! I am monica"); 
+        
     }
 }
